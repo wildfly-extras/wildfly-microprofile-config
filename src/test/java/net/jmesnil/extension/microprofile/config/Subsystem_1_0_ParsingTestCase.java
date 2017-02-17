a969771a59effe1a52a1fee9fb93f5e2003cf511
Jeff Mesnil <jmesnil@gmail.com>
2017-02-17 17:09:45 +0100

    initial commit

---
+++ src/test/java/net/jmesnil/extension/microprofile/config/Subsystem_1_0_ParsingTestCase.java
@@ -0,0 +1,31 @@
+package net.jmesnil.extension.microprofile.config;
+
+import java.io.IOException;
+
+import org.jboss.as.subsystem.test.AbstractSubsystemBaseTest;
+
+/**
+ * This is the bare bones test example that tests subsystem
+ * It does same things that {@link SubsystemParsingTestCase} does but most of internals are already done in AbstractSubsystemBaseTest
+ * If you need more control over what happens in tests look at  {@link SubsystemParsingTestCase}
+ * @author <a href="mailto:tomaz.cerar@redhat.com">Tomaz Cerar</a>
+ */
+public class Subsystem_1_0_ParsingTestCase extends AbstractSubsystemBaseTest {
+
+    public Subsystem_1_0_ParsingTestCase() {
+        super(SubsystemExtension.SUBSYSTEM_NAME, new SubsystemExtension());
+    }
+
+
+    @Override
+    protected String getSubsystemXml() throws IOException {
+        return readResource("subsystem_1_0.xml");
+    }
+
+    @Override
+    protected String getSubsystemXsdPath() throws IOException {
+        return "schema/microprofile-config-extension_1_0.xsd";
+    }
+
+
+}
