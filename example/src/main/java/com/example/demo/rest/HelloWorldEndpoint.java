fa90d2da366512b62fd8f48aeb36b1b9b87c7d4a
Jeff Mesnil <jmesnil@gmail.com>
2017-04-14 10:18:02 +0200

    add fraction and example to the project

---
+++ example/src/main/java/com/example/demo/rest/HelloWorldEndpoint.java
@@ -0,0 +1,25 @@
+package com.example.demo.rest;
+
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.config.Config;
+
+
+@Path("/hello")
+public class HelloWorldEndpoint {
+
+	@Inject
+	Config config;
+
+	@GET
+	@Produces("text/plain")
+	public Response doGet() {
+		String text = "FOO_BAR property is " + config.getOptionalValue("FOO_BAR", String.class);
+		return Response.ok(text).build();
+	}
+}
